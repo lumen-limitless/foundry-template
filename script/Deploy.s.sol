3e8a5ec73a9440cf0aa88e4ae662a1cf34b10a83
lumen-limitless <lumenlimitless@gmail.com>
2023-02-13 18:10:29 -0500

    updated config added example deploy script

---
+++ script/Deploy.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Contract} from "src/Contract.sol";
+import "forge-std/Script.sol";
+
+contract DeployContract is Script {
+    function run() external returns (Contract c) {
+        vm.startBroadcast();
+
+        c = new Contract(1);
+
+        vm.stopBroadcast();
+    }
+}
